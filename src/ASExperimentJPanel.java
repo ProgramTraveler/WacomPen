c4ce29785106c049ab751af2d3f6d86103f6dd02
PrpgramTraveler <2115655391@qq.com>
2021-02-03 11:43:28 +0800

    2021-02-03

---
+++ src/ASExperimentJPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+/*
+    date:2021-02-02
+    author:王久铭
+    purpose:方位角离散化界面，主要是将方位角进行动态显示，专门为A-离散化使用
+ */
+public class ASExperimentJPanel extends JPanel {
+}
---
+++ src/ScatteredAzimuth.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.event.*;
+
+/*
+    date:2021-02-02
+    author:王久铭
+    purpose:方位角离散化界面，主要是通过方位角的不断变化来进行颜色和像素的选择
+ */
+public class ScatteredAzimuth extends JFrame implements MouseInputListener, KeyListener, ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
